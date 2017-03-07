2b25ab52a02cb023d6d9a76c3e1f4db3693f3840
mSeifried <seifried.mat@gmail.com>
2017-03-07 17:09:11 -0500

    Initial Commit
    Setup initial document as a single page website. The website will
    eventually be built out into a multipage website.

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<!-- Project Comments -->
+		<!-- This website features a small property maintenance company which is local to me. This website is targeting homeowners and small businesses requiring property maintenance either on a as needed or regular basis. -->
+		<!-- Original Website: http://www.propertyworkz.ca/about.html -->
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Property Workz - Professional Property Maintenance</title>
+
+		<meta name="description" content="Property maintenance for less. Professional property care for residential and commercial needs covering a range of services.">
+		<meta name="keywords" content="Property Maintenance, Grass Cutting, Lawn Mowing, Weed Whipping, Tree Removal, Stump Removal">
+		<meta name="author" content="Mathew Seifried">
+	</head>
+
+	<body>
+		<header>
+			<nav role="navigation">
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="services">Services</a></li>
+					<li><a href="work">Our Work</a></li>
+					<li><a href="contact">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<div class="slideshow" role="banner">
+
+		</div>
+
+		<main role="main">
+			<section id="services">
+				<h2>Services</h2>
+
+				<div class="service-block">
+					<h3>Lawn Care</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+
+				<div class="service-block">
+					<h3>Gardening</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+
+				<div class="service-block">
+					<h3>Power Washing</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+			</section>
+
+			<section id="service-areas">
+				<h2>Serving;</h2>
+				<ul>
+					<li>Brampton</li>
+					<li>Caledon</li>
+					<li>Erin</li>
+					<li>Georgetown</li>
+					<li>Guelph</li>
+					<li>Orangeville</li>
+					<li>and surrounding areas!</li>
+				</ul>
+			</section>
+		</main>
+
+		<section id="testimonials" role="complementary">
+			<h2>Testimonials</h2>
+
+			<blockquote cite="John Doe" class="testimonial-slider">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br> - John Doe
+			</blockquote>
+		</section>
+
+		<footer role="contentinfo">
+			<small>&copy; <?php echo date("Y"); ?> Property Workz</small>
+			<small>Website By: <a href="www.mathew.codes">Mathew Seifried</a></small>
+		</footer>
+	</body>
+</html>
